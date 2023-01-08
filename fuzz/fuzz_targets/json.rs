8e05b0b17581431bfbc35f798e02e416889246bf
Ryan Fowler <ryan.fowler19@gmail.com>
2023-01-08 14:03:31 +0000

    Fix fuzz targets

--- fuzz/fuzz_targets/json.rs
+++ fuzz/fuzz_targets/json.rs
@@ -4,7 +4,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        if let Ok(out) = fjson::format_json_compact(s) {
+        if let Ok(out) = fjson::to_json_compact(s) {
             match serde_json::from_str::<serde_json::Value>(&out) {
                 Ok(_) => {}
                 Err(err) => {
@@ -17,6 +17,10 @@ fuzz_target!(|data: &[u8]| {
                     }
                     panic!("{}", err);
                 }
+            }
+        } else {
+            if let Ok(_) = serde_json::from_slice::<serde_json::Value>(data) {
+                panic!("serde_json could parse input that fjson could not");
             }
         }
     }
